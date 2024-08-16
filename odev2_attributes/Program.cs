29744ac526e6099832600f93b885ada3d83ad299
Abdullah <abdullahtopal0@gmail.com>
2024-08-17 00:56:02 +0300

    console app attributes project with the class library

---
+++ odev2_attributes/Program.cs
@@ -0,0 +1,41 @@
+﻿using AttributesLibrary;
+
+namespace odev2_attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci ogrenci = new Ogrenci
+            {
+                adi = "Abdullah",
+                soyadi = "Topal",
+                bolum = "Bilgisayar Mühendisliği"
+            };
+
+            Araba araba = new Araba
+            {
+                marka = "BMW",
+                model = "2012"
+            };
+
+            if (!ZorunlulukKontrolu.Dogrula(ogrenci))
+            {
+                Console.WriteLine("Bilgilerin girilmesi zorunludur");
+            }
+            else
+            {
+                Console.WriteLine("Form başarılı");
+            }
+
+            if (!ZorunlulukKontrolu.Dogrula(araba))
+            {
+                Console.WriteLine("Araba bilgileri eksik");
+            }
+            else
+            {
+                Console.WriteLine("Araba bilgileri tamam");
+            }
+        }
+    }
+}
